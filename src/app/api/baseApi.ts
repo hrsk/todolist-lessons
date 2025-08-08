6023d3c1f830a28639c30dffb7d9b96ed13f2f04
hrsk <hrsk.yegor@gmail.com>
2025-08-08 19:50:58 +0300

    fix: test rebase

---
+++ src/app/api/baseApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { AUTH_TOKEN } from "@/common/constants"
+import { handleError } from "@/common/utils/handleError.ts"
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  tagTypes: ["Todolist", "Task"],
+  baseQuery: async (args, api, extraOptions) => {
+    const response = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      prepareHeaders: (headers) => {
+        headers.set("API-KEY", import.meta.env.VITE_API_KEY)
+        headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+      },
+    })(args, api, extraOptions)
+
+    handleError(api, response)
+
+    return response
+  },
+  endpoints: () => ({}),
+})
