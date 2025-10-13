6ff1d12b410a575d5bdc81bed3f85ca19d43c6b6
hrsk <hrsk.yegor@gmail.com>
2025-10-13 20:26:37 +0300

    feat: app-slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
-import { appReducer } from "./app-reducer"
 import { tasksReducer } from "@/features/todolists/model/tasks-reducer"
 import { todolistsReducer } from "@/features/todolists/model/todolists-reducer"
+import { appReducer, appSlice } from "@/app/app-slice.ts"
 
 // объединение reducer'ов с помощью combineReducers
 const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolists: todolistsReducer,
-  app: appReducer,
+  [appSlice.name]: appReducer,
 })
 
 // создание store
