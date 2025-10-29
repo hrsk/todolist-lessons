37c974f7920c293effae40530b1031cad939fff9
hrsk <hrsk.yegor@gmail.com>
2025-10-29 18:35:47 +0300

    feat: back to homepage button

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,23 @@
 import styles from "./PageNotFound.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
+import { PropsWithChildren } from "react"
 
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+    <Button component={Link} to={"/"}>
+      BACK TO HOMEPAGE
+    </Button>
+    <Button component={CustomLink} href={"/"}>
+      BACK TO HOMEPAGE
+    </Button>
+    <Button href="/" target="_blank" variant="outlined">
+      BACK TO HOMEPAGE
+    </Button>
+    ;
   </>
 )
+
+const CustomLink = (props: PropsWithChildren) => <a {...props}>{props.children}</a>
