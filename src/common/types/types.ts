475117017af42315fc35e6387f47490c7de14ab2
hrsk <hrsk.yegor@gmail.com>
2025-10-30 18:17:53 +0300

    feat: tasks zod validation

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,14 +1,24 @@
-export type FieldError = {
-  error: string
-  field: string
-}
+import { z } from "zod/v4"
+import { ResultCode } from "@/common/enums"
 
-export type BaseResponse<T = {}> = {
-  data: T
-  resultCode: number
-  messages: string[]
-  fieldsErrors: FieldError[]
-}
+// export type FieldError = {
+//   error: string
+//   field: string
+// }
+
+export const fieldErrorSchema = z.object({
+  error: z.string().nullable(),
+  field: z.string().nullable(),
+})
+
+export type FieldError = z.infer<typeof fieldErrorSchema>
+
+// export type BaseResponse<T = {}> = {
+//   data: T
+//   resultCode: number
+//   messages: string[]
+//   fieldsErrors: FieldError[]
+// }
 
 export type RequestStatus = "idle" | "pending" | "succeeded" | "failed"
 
@@ -19,3 +29,23 @@ export type Nullable<T> = T | null
 //   password: string
 //   rememberMe: boolean
 // }
+
+// const baseResponseSchema = function createBaseResponseSchema<T extends z.ZodType>(schema: T): T {
+//   return z.object({
+//     data: z.array(schema),
+//     resultCode: z.number(),
+//     messages: z.array(z.string()),
+//     fieldsErrors: z.array(fieldErrorSchema),
+//   })
+// }
+
+export const createBaseResponseSchema = <T extends z.ZodTypeAny>(schema: T) => {
+  return z.object({
+    data: schema,
+    resultCode: z.enum(ResultCode),
+    messages: z.string().array(),
+    fieldsErrors: z.array(fieldErrorSchema),
+  })
+}
+
+export type BaseResponse = z.infer<typeof createBaseResponseSchema>
