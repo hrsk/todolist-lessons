05796c98ba13f0d5cf651e5443c05e1a70e9afad
hrsk <hrsk.yegor@gmail.com>
2025-11-06 16:39:55 +0300

    feat(api): implement logic get todos with using RTK Query

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -1,28 +1,38 @@
-import { useAppDispatch, useAppSelector } from "@/common/hooks"
-import { fetchTodos, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
 import Grid from "@mui/material/Grid2"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
+import { useGetTodosQuery } from "@/features/todolists/api/todolistsApi.ts"
+import styles from "@/app/App.module.css"
+import { CircularProgress } from "@mui/material"
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
+  // const todolists = useAppSelector(selectTodolists)
 
-  const dispatch = useAppDispatch()
+  const { data: todolists, isFetching } = useGetTodosQuery()
+  // const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    dispatch(fetchTodos())
-  }, [])
+  // useEffect(() => {
+  // dispatch(fetchTodos())
+  // }, [])
+
+  if (isFetching) {
+    return (
+      <div className={styles.circularProgress}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
 
   return (
     <>
-      {todolists.map((todolist) => (
-        <Grid key={todolist.id}>
-          <Paper sx={{ p: "0 20px 20px 20px" }}>
-            <TodolistItem todolist={todolist} />
-          </Paper>
-        </Grid>
-      ))}
+      {todolists &&
+        todolists.map((todolist) => (
+          <Grid key={todolist.id}>
+            <Paper sx={{ p: "0 20px 20px 20px" }}>
+              <TodolistItem todolist={todolist} />
+            </Paper>
+          </Grid>
+        ))}
     </>
   )
 }
