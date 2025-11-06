7c4de6dd52e3cf7480af14a39e8f846594d4e75a
hrsk <hrsk.yegor@gmail.com>
2025-11-06 16:39:55 +0300

    feat(api): implement logic get todos with using RTK Query

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,23 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice"
-import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice"
 import { appReducer, appSlice } from "@/app/app-slice.ts"
 import { authReducer, authSlice } from "@/features/auth/model/auth-slice.ts"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { setupListeners } from "@reduxjs/toolkit/query"
 
 export const store = configureStore({
-  reducer: combineReducers({
+  reducer: {
+    [todolistsApi.reducerPath]: todolistsApi.reducer,
     [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
+    // [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
-  }),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todolistsApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,19 +1,43 @@
-import { instance } from "@/common/instance"
-import type { BaseResponse } from "@/common/types"
-import type { Todolist } from "./todolistsApi.types"
+import type { DomainTodolist, Todolist } from "./todolistsApi.types"
 
-export const todolistsApi = {
-  getTodolists() {
-    return instance.get<Todolist[]>("/todo-lists")
-  },
-  changeTodolistTitle(payload: { id: string; title: string }) {
-    const { id, title } = payload
-    return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
-  },
-  createTodolist(title: string) {
-    return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
-  },
-  deleteTodolist(id: string) {
-    return instance.delete<BaseResponse>(`/todo-lists/${id}`)
-  },
-}
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { AUTH_TOKEN } from "@/common/constants"
+
+export const todolistsApi = createApi({
+  reducerPath: "todolists",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://social-network.samuraijs.com/api/1.1`,
+    headers: {
+      "API-KEY": import.meta.env.VITE_API_KEY,
+    },
+    prepareHeaders: (headers) => headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`),
+  }),
+  endpoints: (build) => ({
+    getTodos: build.query<DomainTodolist[], void>({
+      query: () => `todo-lists`,
+      transformResponse: (response: Todolist[]): DomainTodolist[] => {
+        return response.map((todolist) => ({ ...todolist, filter: "all" }))
+      },
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetTodosQuery } = todolistsApi
+//
+// export const _todolistsApi = {
+//   getTodolists() {
+//     return instance.get<Todolist[]>("/todo-lists")
+//   },
+//   changeTodolistTitle(payload: { id: string; title: string }) {
+//     const { id, title } = payload
+//     return instance.put<BaseResponse>(`/todo-lists/${id}`, { title })
+//   },
+//   createTodolist(title: string) {
+//     return instance.post<BaseResponse<{ item: Todolist }>>("/todo-lists", { title })
+//   },
+//   deleteTodolist(id: string) {
+//     return instance.delete<BaseResponse>(`/todo-lists/${id}`)
+//   },
+// }
